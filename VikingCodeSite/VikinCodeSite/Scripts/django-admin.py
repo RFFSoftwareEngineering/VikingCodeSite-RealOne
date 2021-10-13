1cb005803047721a4b524c2bd43a64df96a9574c
Rafael Fodra Furlan <Rafael_Viking_Code@outlook.com>
2021-10-13 18:20:01 -0300

    initial commit

---
+++ VikingCodeSite/VikinCodeSite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Rafae\source\repos\VikingCodeSite\VikingCodeSite\VikinCodeSite\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
