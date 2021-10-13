bd30f323ee2a797939e8c0508ee6002f76951e14
Rafael Fodra Furlan <Rafael_Viking_Code@outlook.com>
2021-10-13 19:05:03 -0300

    everything fine... now migrate then work on root-page

--- VikingCodeSite/Core/views.py
+++ VikingCodeSite/Core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request):
+    context = {
+                
+    }
+    return render(request, "index.html", context)
