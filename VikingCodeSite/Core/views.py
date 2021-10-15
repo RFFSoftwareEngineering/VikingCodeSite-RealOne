f36bf5cab1f330a9f97daa57ea7ad189c103065e
Rafael Fodra Furlan <Rafael_Viking_Code@outlook.com>
2021-10-15 01:09:23 -0300

    the site is getting coool!

--- VikingCodeSite/Core/views.py
+++ VikingCodeSite/Core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+import time
 
 
 def index(request):
@@ -7,3 +8,10 @@ def index(request):
                 
     }
     return render(request, "index.html", context)
+
+def home(request):
+    context = {
+        
+    }
+    time.sleep(1.25)
+    return render(request, "home.html", context)
