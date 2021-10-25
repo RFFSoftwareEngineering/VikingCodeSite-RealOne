376dcec45ca55606972ecace3798a318fdf8e268
Rafael Fodra Furlan <Rafael_Viking_Code@outlook.com>
2021-10-24 21:25:41 -0300

    good slidebar made

--- VikingCodeSite/Core/Static/javascript/animtheme.js
+++ VikingCodeSite/Core/Static/javascript/animtheme.js
@@ -1,4 +1,8 @@
 themebtn.addEventListener('click', () => {
     bodyid.classList.toggle('animate');
-    topbar.classList.toggle('animate');
+    topbar.classList.toggle('animate');    
+});
+
+berserkerslide.addEventListener('click', () => {
+    ModalSlider.classList.toggle('animate1');
 });
