52156e59c59bfdb16018c458733716337cd26c08
Rafael Fodra Furlan <Rafael_Viking_Code@outlook.com>
2021-10-26 00:31:03 -0300

    corrected server time

--- VikingCodeSite/VikingCodeSite/settings.py
+++ VikingCodeSite/VikingCodeSite/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 LANGUAGE_CODE = 'pt-br'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Brazil/East'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
