26a08b86aeb890222e6ece9cd443126657737c11
Rafael Fodra Furlan <Rafael_Viking_Code@outlook.com>
2021-10-25 23:53:00 -0300

    who we are finished. i hope community enjoy and clients of course

--- VikingCodeSite/Core/Static/javascript/animtheme.js
+++ VikingCodeSite/Core/Static/javascript/animtheme.js
@@ -8,4 +8,11 @@ themebtn.addEventListener('click', () => {
     bodyid.classList.toggle('animate');
     topbar.classList.toggle('animate');
     sidebar.classList.toggle('animate');
+    VkAp.classList.toggle('animate3');
+    QuemSomos.classList.toggle('animate4');
+    mentalidadevk.classList.toggle('animate3');
+    vkmen.classList.toggle('animate4');
+    gestaoexplanation.classList.toggle('animate4');
+    Proposito.classList.toggle('animate3');
+    purposetxt.classList.toggle('animate4');
 });
